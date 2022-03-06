cb0b20cfa582879254327234faaa9defd56eabc3
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2022-03-06 12:53:36 +0530

    Tutorial 2

---
+++ Tutorial 2/ex2.cpp
@@ -0,0 +1,78 @@
+/*/*Exercise 2 - Selection
+
+Convert the C program given below which calculates an employee's salary to a C++ program.
+
+Input Type, Salary, otHours
+
+Type = 1
+OtRate = 1000
+Type = 2
+OtRate = 1500
+Type = 3
+OtRate = 1700
+Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
+
+/*#include <stdio.h>
+int main()
+{
+   double salary, netSalary;
+   int etype, otHrs, otRate;
+   printf("Enter Employee Type : ");
+   scanf("%d", &etype);
+   printf("Enter Salary  : ");
+   scanf("%f", &salary);
+   printf("Enter OtHrs : ");
+   scanf("%d", &otHrs); 
+   
+   switch (etype) {
+      case 1 :
+          otRate = 1000;
+          break;
+      case 2 :
+          otRate = 1500;
+          break;
+      default :
+          otRate = 1700;
+          break;
+   }
+
+
+   netSalary = salary + otHrs* otRate;
+   printf("Net Salary is %f ", netSalary);
+  
+   return 0;
+}*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  double salary, netSalary;
+  int etype,otHrs,otRate;
+
+  cout<<"Enter Employe Type : ";
+  cin>>etype;
+  cout<<"Enter Salary : ";
+  cin>>salary;
+  cout<<"Enter otHrs : ";
+  cin>>otHrs;
+
+  switch (etype) {
+      case 1 :
+          otRate = 1000;
+          break;
+      case 2 :
+          otRate = 1500;
+          break;
+      default :
+          otRate = 1700;
+          break;
+   }
+
+   netSalary = salary + otHrs* otRate;
+
+   cout<<"net Salary is ";cout<<netSalary;
+
+}
---
+++ Tutorial 2/ex4.cpp
@@ -0,0 +1,40 @@
+/*Exercise 4 - Functions
+
+Write a program to calculate the function called nCr which is defined as
+
+nCr = n!/ r!(n−r)!
+Where n! is the factorial of n.
+
+Implement the functions
+
+long Factorial(int no);
+long nCr(int n, int r);
+Do not modify the main function.*/
+
+#include <iostream>
+
+long Factorial(int no);
+long nCr(int n, int r);
+
+int main() {
+  int n, r;
+  std::cout << "Enter a value for n ";
+  std::cin >> n;
+  std::cout << "Enter a value for r ";
+  std::cin >> r;
+  std::cout << "nCr = ";
+  std::cout << nCr(n,r);
+  std::cout << std::endl;
+  return 0;
+}
+
+long nCr(int n, int r) {
+  long result = 1;
+  for (int i = n; i > n - r; i--) {
+    result *= i;
+  }
+  for (int i = 1; i <= r; i++) {
+    result /= i;
+  }
+  return result;
+}
