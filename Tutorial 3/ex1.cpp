012118ee4bd91f25aaa288377f253db06e58a460
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2022-03-06 12:56:13 +0530

    Tutorial 3

---
+++ Tutorial 3/ex1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<iomanip> //Define iomanip library for formatting
+
+using namespace std;
+int main() {
+ float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
+ char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
+ cout <<setw(5)<< "No" <<setw(15)<< "Name" <<setw(10)<< "Marks" << endl; 
+ for (int r = 0; r < 5; r++) {
+     cout<<setw(5) <<  r+1 
+          <<setw(15)<<  names[r]
+          <<setw(10)<<setprecision(2)<<setiosflags(ios::fixed)<< marks[r] << endl;
+ }
+}
