3409389579b2e16e37ee9e5a0b86cb50dcf1432f
Janith Sandamal <2000janith@gmail.com>
2022-03-12 02:55:21 +0530

    complete

--- Tutorial 3/ex1.cpp
+++ Tutorial 3/ex1.cpp
@@ -7,7 +7,7 @@ int main() {
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
  cout <<setw(5)<< "No" <<setw(15)<< "Name" <<setw(10)<< "Marks" << endl; 
  for (int r = 0; r < 5; r++) {
-     cout<<setw(5) <<  r+1 
+     cout<<setw(5) <<  r+1  
           <<setw(15)<<  names[r]
           <<setw(10)<<setprecision(2)<<setiosflags(ios::fixed)<< marks[r] << endl;
  }
